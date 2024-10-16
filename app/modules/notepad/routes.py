33926e2b1f28a3d566f03436c75db30b6e0e70d3
Felipe Solis Agudo <felipesolisagudo@gmail.com>
2024-10-16 11:42:04 +0200

    CRUD bien

--- app/modules/notepad/routes.py
+++ app/modules/notepad/routes.py
@@ -7,15 +7,24 @@ from app.modules.notepad.services import NotepadService
 
 notepad_service = NotepadService()
     
-    
+'''
+READ ALL
+'''
+
+
 @notepad_bp.route('/notepad', methods=['GET'])
 @login_required
 def index():
     form = NotepadForm()
     notepads = notepad_service.get_all_by_user(current_user.id)
     return render_template('notepad/index.html', notepads=notepads, form=form)
-    
-    
+
+   
+'''
+CREATE
+'''
+
+
 @notepad_bp.route('/notepad/create', methods=['GET', 'POST'])
 @login_required
 def create_notepad():
@@ -33,6 +42,11 @@ def create_notepad():
     return render_template('notepad/create.html', form=form)
 
 
+'''
+READ BY ID
+'''
+
+
 @notepad_bp.route('/notepad/<int:notepad_id>', methods=['GET'])
 @login_required
 def get_notepad(notepad_id):
@@ -43,6 +57,11 @@ def get_notepad(notepad_id):
         return redirect(url_for('notepad.index'))
 
     return render_template('notepad/show.html', notepad=notepad)
+
+
+'''
+EDIT
+'''
 
 
 @notepad_bp.route('/notepad/edit/<int:notepad_id>', methods=['GET', 'POST'])
@@ -71,6 +90,11 @@ def edit_notepad(notepad_id):
     return render_template('notepad/edit.html', form=form, notepad=notepad)
 
 
+'''
+DELETE
+'''
+
+
 @notepad_bp.route('/notepad/delete/<int:notepad_id>', methods=['POST'])
 @login_required
 def delete_notepad(notepad_id):
@@ -86,3 +110,4 @@ def delete_notepad(notepad_id):
         flash('Error deleting notepad', 'error')
     
     return redirect(url_for('notepad.index'))
+
--- app/modules/notepad/services.py
+++ app/modules/notepad/services.py
@@ -1,7 +1,6 @@
 from app.modules.notepad.repositories import NotepadRepository
 from core.services.BaseService import BaseService
-from app.modules.notepad.models import Notepad
-from core.repositories.BaseRepository import BaseRepository
+
 
 class NotepadService(BaseService):
     def __init__(self):
@@ -9,13 +8,4 @@ class NotepadService(BaseService):
 
     def get_all_by_user(self, user_id):
         return self.repository.get_all_by_user(user_id)
-    
-    
-    
-class NotepadRepository(BaseRepository):
-    def __init__(self):
-        super().__init__(Notepad)
-
-    def get_all_by_user(self, user_id):
-        return Notepad.query.filter_by(user_id=user_id).all()
 
